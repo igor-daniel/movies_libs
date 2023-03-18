68ddeea305ea8fc93d71e7439b208042e5c70a2c
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-18 16:57:47 -0300

    Requisição fetch sendo feita

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from 'react'
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 export const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results)
+  };
+  useEffect(() => {
+    const topRetedURL = `${moviesURL}top_rated?${apiKey}`
+    getTopRatedMovies(topRetedURL)
+  }, [])
   return (
-    <h2>Home</h2>
+    <div>
+      {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>
   )
-}
+};
