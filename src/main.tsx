c78ffe100d9f1ff32a6825f358db4395294d7f93
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-18 02:30:06 -0300

    Rotas Criadas com sucesso

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Movie } from './pages/Movie'
+import { Search } from './pages/Search'
+
+
+
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='movie/:id' element={<Movie />} ></Route>
+          <Route path='search' element={<Search />} ></Route>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
