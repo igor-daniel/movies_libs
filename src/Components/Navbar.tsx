5a6f1be48a641ca7766235081afa52a7663c7c63
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-25 15:58:34 -0300

    erro no search

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,15 +1,31 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 import './navBar.css'
 
 export const Navbar = () => {
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!search) return;
+    navigate(`/search?q=${search}`, { replace: true })
+    setSearch('')
+  }
+
   return (
     <nav id='navbar'>
       <h2>
         <Link to="/"> <BiCameraMovie /> MoviesLib </Link>
       </h2>
-      <form>
-        <input type="text" placeholder='busque um filme' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='busque um filme'
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type='submit'> <BiSearchAlt2 /></button>
       </form>
     </nav>
